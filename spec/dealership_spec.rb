0b684b3788e5cbb5cb16d0216821550a26f77927
Ronando Long <ronando.long@gmail.com>
2015-08-14 01:06:46 -0700

    It will initialize, name spec passes.

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -1,2 +1,24 @@
 require('rspec')
 require('dealership')
+
+describe('Dealership') do
+  # before() do
+  #   Dealership.clear()
+  # end
+
+  describe('#name') do
+    it('returns name of the dealership from array') do
+      test_dealership = Dealership.new("Bob's")
+      expect(test_dealership.name()).to(eq("Bob's"))
+    end
+  end
+
+
+
+
+
+
+
+
+
+end

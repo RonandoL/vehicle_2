83755fd0413d5352fb7a73d99e8b1fc5d24ba733
Ronando Long <ronando.long@gmail.com>
2015-08-14 01:32:18 -0700

    Will return array of dealerships, will clear array

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -2,10 +2,26 @@ require('rspec')
 require('dealership')
 
 describe('Dealership') do
-  # before() do
-  #   Dealership.clear()
-  # end
+  before() do
+    Dealership.clear()
+  end
 
+# Class Methods -----------------------
+  describe('.all') do
+    it('returns the array of dealerships') do
+      expect(Dealership.all()).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it('clears out saved dealerships in array') do
+      Dealership.new("Kelly's Kars").save()
+      Dealership.clear()
+      expect(Dealership.all()).to(eq([]))
+    end
+  end
+
+# Instance Methods -----------------------
   describe('#name') do
     it('returns name of the dealership from array') do
       test_dealership = Dealership.new("Bob's")
@@ -26,6 +42,19 @@ describe('Dealership') do
       expect(test_dealership.cars()).to(eq([]))
     end
   end
+
+
+
+  describe('#save') do
+    it('will add a dealership to array of dealerships') do
+      test_dealership = Dealership.new("Bob's")
+      test_dealership.save()
+      expect(Dealership.all()).to(eq([test_dealership]))
+    end
+  end
+
+
+
 
 
 

5dc4422227b3b89d688434ff4fcd4e007f684a66
Ronando Long <ronando.long@gmail.com>
2015-08-13 23:48:12 -0700

    Create vehicles.erb and vehicles_form.erb pages. Route successful from homepage

--- app.rb
+++ app.rb
@@ -7,11 +7,13 @@ get('/') do
   erb(:index)
 end
 
-get('vehicles') do
-  @vehicles = Vehicle.all()  # server collects all the vehicles (Vehicle.all)
-  erb(:vehicles)
-end
+# All Vehicles Page
+  get('/vehicles') do
+    @vehicles = Vehicle.all()  # server collects all the vehicles (Vehicle.all)
+    erb(:vehicles)
+  end
 
-get('vehicles/new') do   # form to enter new vehicle, note URL route diff from form name
-  erb(:vehicles_form)
-end
+# Vehicle Form Page
+  get('/vehicles/new') do   # form to enter new vehicle, note URL route diff from form name
+    erb(:vehicles_form)
+  end
